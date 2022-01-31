47f4d0285d96396c1365a4d6912a8eca500e76f3
mangwu <1185956753@qq.com>
2022-01-31 23:11:33 +0800

    leetcode1342

---
+++ 20220131/leetcode1342. 将数字变成 0 的操作次数/main.js
@@ -0,0 +1,45 @@
+// 给你一个非负整数 num ，请你返回将它变成 0 所需要的步数。 如果当前数字是偶数，你需要把它除以 2 ；否则，减去 1 。
+
+
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+	let ans = 0;
+	while(num !== 0) {
+		if (num % 2 === 0) {
+			num = num / 2;
+		} else {
+			num--;
+		}
+		ans++;
+	}
+	return ans;
+};
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps2 = function(num) {
+	
+	// 以二进制看问题
+	// 实际上每次将偶数除以2相当于当末尾为0时，数字右移一位
+	// 而奇数时减去一相当于某位为1时，数字减去1，变为偶数
+	// 所以总次数等于数字最高位位置数和二机制为1的个数
+
+	/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    if(num == 0)
+        return 0
+    let n = num
+    n = n - ((n >>> 1) & 0x55555555)
+    n = (n & 0x33333333) + ((n >>> 2) & 0x33333333)
+    n = (n + (n >>> 4)) & 0x0F0F0F0F
+    return ((n * 0x01010101) >>> 24) + Math.floor(Math.log2(num))
+};
