b8169e9866d2430231f8af8b88d80aa642ce4f3c
mangwu <1185956753@qq.com>
2022-02-07 14:11:36 +0800

    leetcode1405

--- 20220207/leetcode1405. 最长快乐字符串/main.js
+++ 20220207/leetcode1405. 最长快乐字符串/main.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: main.js                                                              *
  * @Date: 2022-02-07 08:51:58                                                  *
- * @LastModifiedDate: 2022-02-07 10:51:45                                      *
+ * @LastModifiedDate: 2022-02-07 13:56:30                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2022                                                          *
@@ -81,4 +81,53 @@ var longestDiverseString = function (a, b, c) {
     }
   }
   return ans;
+};
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {string}
+ */
+var longestDiverseString = function (a, b, c) {
+  // 上述方法在每次遍历后都手动计算最大和第二大的值，计算过程繁琐
+  // 可以使用数组的排序方法将a, b,c 三个字符进行排序，从而每次遍历时都能获得最大和第二大的值
+  // 用数组保存结果
+  const res = [];
+  // 用于根据，a,b,c大小进行排序的数组
+  const arr = [['a', a], ['b', b], ['c', c]];
+  // 由于可以根据res的最后一位判断上一次保存的字符是什么，所以不必声明使用current
+  // 开始遍历
+  while(true) {
+    // 每次都进行数组排序，取得最多和第二多字符
+    arr.sort((a, b) => b[1] - a[1]);
+    // 声明本轮循环后是否能进入到下一轮的变量
+    // 它可以通过每轮贪心中最多的或者第二多那一个字符数量是否被添加来判断下一轮是否继续
+    let hasNext = false;
+    // 遍历数组，每次都取数量最多的为本轮字符，如果连续选了两次，就选择第二个字符
+    for (let [i, [ch, cnum]] of arr.entries()) {
+      // 当其值小于等于0，说明第一个或者第二个大的值无法提供字符，可以结束字符选择了
+      if (cnum <= 0) {
+        break;
+      }
+      // 判断本次是否选择字符最多的字符
+      const len = res.length;
+      if (len >= 2 && res[len - 2] === ch && res[len - 1] === ch) {
+        // 后面的两个字符都和本轮字符相同，则选择第二个
+        continue;
+      }
+      // 选择第一个或者第二个
+      hasNext = true;
+      res.push(ch);
+      // 更新数组
+      arr[i][1]--;
+      // 进行下一轮选择
+      break;
+    }
+    // 如果没有下一次，退出循环
+    if (!hasNext) {
+      break;
+    }
+  }
+  return res.join("");
 };
