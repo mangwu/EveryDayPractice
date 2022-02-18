b892fc006c53d199fd451f2f49a84e16bf633446
mangwu <1185956753@qq.com>
2022-02-18 11:20:37 +0800

    leetcode688

--- 20220217/leetcode688. 骑士在棋盘上的概率/main.js
+++ 20220217/leetcode688. 骑士在棋盘上的概率/main.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: main.js                                                              *
  * @Date: 2022-02-17 08:38:53                                                  *
- * @LastModifiedDate: 2022-02-17 20:07:28                                      *
+ * @LastModifiedDate: 2022-02-18 11:10:06                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2022 mangwu                                                   *
@@ -85,7 +85,7 @@ knightProbability(3, 2, 0, 0);
  * @return {number}
  */
 var knightProbability2 = function (n, k, row, column) {
-  console.log(dfs(n, k, row, column));
+  // console.log(dfs(n, k, row, column));
   return dfs(n, k, row, column);
 };
 // 暴力dfs
@@ -100,7 +100,7 @@ const dfs = (n, k, i, j) => {
   }
   // 遍历每个方向，每个路径概率均等，都为 1 / 8
   let ans = 0;
-  for (let x = 0; i < DIRS.length; i++) {
+  for (let x = 0; x < DIRS.length; x++) {
     const dir = DIRS[x];
     // 开始每条路径概率在前一个路径基础上乘以 1 / 8
     // 总路径概率之和就为总之结果
@@ -124,35 +124,81 @@ var knightProbability3 = function (n, k, row, column) {
   // 三维数组
   const cache = new Array(n)
     .fill(-1)
-    .map(() => new Array(n).fill(-1).map(() => new Array(k).fill(-1)));
-  console.log(dfsWithCache(n, k, row, column, cache));
+    .map(() => new Array(n).fill(-1).map(() => new Array(k + 1).fill(-1)));
+  // console.log(dfsWithCache(n, k, row, column, cache));
+  // 记忆dfs
+  const dfsWithCache = (n, k, i, j) => {
+    console.log("------dfs--------");
+    // 跳到棋盘外不会增加概率，直接返回0
+    if (i < 0 || j < 0 || i >= n || j >= n) {
+      return 0;
+    }
+    // 步数跳完，不用再递归，直接返回1的概率(当前不用再选择，在路径上的概率称1即可)
+    if (k == 0) {
+      return 1;
+    }
+    // console.log("----dfs----");
+    // 缓存中存在就返回
+    if (cache[i][j][k] != -1) {
+      // console.log(cache[i][j][k], i, j, k);
+      return cache[i][j][k];
+    }
+    // 遍历每个方向，每个路径概率均等，都为 1 / 8
+    let ans = 0;
+    console.log(i, j, k);
+    for (let x = 0; x < DIRS.length; x++) {
+      const dir = DIRS[x];
+      // 开始每条路径概率在前一个路径基础上乘以 1 / 8
+      // 总路径概率之和就为总之结果
+      ans = ans + dfsWithCache(n, k - 1, i + dir[0], j + dir[1]) / 8.0;
+    }
+    console.log(ans);
+    cache[i][j][k] = ans;
+    return ans;
+  };
   return dfsWithCache(n, k, row, column, cache);
 };
 
-// 记忆dfs
-const dfsWithCache = (n, k, i, j, cache) => {
-  // 跳到棋盘外不会增加概率，直接返回0
-  if (i < 0 || j < 0 || i >= n || j >= n) {
-    return 0;
+const ans = knightProbability3(3, 2, 0, 0);
+console.log(ans);
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @param {number} row
+ * @param {number} column
+ * @return {number}
+ */
+var knightProbability3 = function (n, k, row, column) {
+  // 动态规划
+  // 声明dp
+  const dp = new Array(n)
+    .fill(-1)
+    .map(() => new Array(n).fill(-1).map(() => new Array(k + 1).fill(0)));
+  // k从0开始变大
+  for (let kk = 0; kk <= k; kk++) {
+    // 遍历棋盘
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n; j++) {
+        // 走0步到[i, j]的概率为1
+        if (kk == 0) {
+          // 即初始时，0步，棋盘上任意位置概率都是1
+          dp[i][j][kk] = 1;
+        } else {
+          // 如果不是0步，则需要从其它地方（8个方向）走过来
+          // 例如如果kk为1, 那么需要8个方向0步的概率，且确保8个方向上的位置在棋盘上
+          for (let x = 0; x < DIRS.length; x++) {
+            let ni = i + DIRS[x][0];
+            let nj = j + DIRS[x][1];
+            if (ni >= 0 && nj >= 0 && ni < n && nj < n) {
+              dp[i][j][kk] += dp[ni][nj][kk - 1] / 8.0;
+            }
+          }
+        }
+      }
+    }
   }
-  // 步数跳完，不用再递归，直接返回1的概率(当前不用再选择，在路径上的概率称1即可)
-  if (k == 0) {
-    return 1;
-  }
-  // 缓存中存在就返回
-  if (cache[i][j][k] != -1) {
-    return cache[i][j][k];
-  }
-  // 遍历每个方向，每个路径概率均等，都为 1 / 8
-  let ans = 0;
-  for (let x = 0; i < DIRS.length; i++) {
-    const dir = DIRS[x];
-    // 开始每条路径概率在前一个路径基础上乘以 1 / 8
-    // 总路径概率之和就为总之结果
-    ans = ans + dfsWithCache(n, k - 1, i + dir[0], j + dir[1], cache) / 8.0;
-  }
-  cache[i][j][k] = ans;
-  return ans;
+  return dp[row][column][k];
 };
 
-knightProbability3(3, 2, 0, 0)
+// 上面动态规划的求法性能耗时比记忆性dfs搜索更多，因为计算了棋盘每个位置的k步的概率
