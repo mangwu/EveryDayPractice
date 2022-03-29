8955de14e6bedf4bd68252ea4628f7d02c2b4faa
mangwu <1185956753@qq.com>
2022-03-29 22:44:34 +0800

    leetcode

--- 20220329/leetcode72. 编辑距离/main.js
+++ 20220329/leetcode72. 编辑距离/main.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: main.js                                                              *
  * @Date: 2022-03-29 21:09:38                                                  *
- * @LastModifiedDate: 2022-03-29 21:09:38                                      *
+ * @LastModifiedDate: 2022-03-29 22:44:23                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2022 mangwu                                                   *
@@ -12,4 +12,11 @@
  * ---------------------	--------	----------------------------------------------- *
  */
 
-
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function (word1, word2) {
+  
+};
