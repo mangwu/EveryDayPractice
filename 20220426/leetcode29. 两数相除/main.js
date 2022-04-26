623e9ca287bce8f4cba36355ecdfab91a4eecfd0
mangwu <1185956753@qq.com>
2022-04-26 23:19:12 +0800

    leetcode

--- 20220426/leetcode29. 两数相除/main.js
+++ 20220426/leetcode29. 两数相除/main.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: main.js                                                              *
  * @Date: 2022-04-26 17:20:31                                                  *
- * @LastModifiedDate: 2022-04-26 17:44:58                                      *
+ * @LastModifiedDate: 2022-04-26 23:18:34                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2022 mangwu                                                   *
@@ -42,7 +42,7 @@ var divide = function (dividend, divisor) {
   // 右移时遗漏的1的个数
   let add = 0;
   while (dividend > divisor) {
-    if (dividend & (1 == 1)) {
+    if ((dividend & 1) == 1) {
       add++;
     }
     dividend = dividend >> 1;
@@ -59,4 +59,61 @@ var divide = function (dividend, divisor) {
   }
   let ans = Math.pow(2, idx) + extra;
   return isNegative ? -ans : ans;
+};
+
+// 上述解答错误
+
+const MAX = Math.pow(2, 31) - 1;
+const MIN = -Math.pow(2, 31);
+/**
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+var divide = function (dividend, divisor) {
+  if (divisor == 1) {
+    return dividend;
+  }
+  if (divisor == -1) {
+    return dividend == MIN ? MAX : -dividend;
+  }
+  let isNegative = false;
+  // 先判断符号
+  if (dividend < 0) {
+    dividend = -dividend;
+    isNegative = !isNegative;
+  } else if (dividend == 0) {
+    return 0;
+  }
+  if (divisor < 0) {
+    divisor = -divisor;
+    isNegative = !isNegative;
+  }
+  // 找出除数的大致范围
+  let idx = -1;
+  let d = divisor;
+  while (d > 0) {
+    d = d >> 1;
+    idx++;
+  }
+  if (1 << idx == divisor) {
+    let ans = Math.abs(dividend >> idx);
+    return isNegative ? -ans : ans;
+  }
+  // dvisor的最高位为idx 右移可能
+  let left = (dividend >> (idx + 1)) + 1;
+  let right = dividend >> idx;
+  // (left, right)
+  while (left <= right) {
+    let mid = (left + right) >> 1;
+    if (mid * divisor == dividend) {
+      return isNegative ? -mid : mid;
+    } else if (mid * divisor > dividend) {
+      // 在区域左边 [ mid - 1]
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return isNegative ? -(left - 1) : left - 1;
 };
