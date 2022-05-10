81200aaf5bd8dd84f75c1fa256cc476275897df5
mangwu <1185956753@qq.com>
2022-05-10 17:22:46 +0800

    leetcode

---
+++ 20220510/leetcode188. 买卖股票的最佳时机 IV/main.js
@@ -0,0 +1,27 @@
+/*
+ * @Author: mangwu                                                             *
+ * @File: main.js                                                              *
+ * @Date: 2022-05-10 11:14:27                                                  *
+ * @LastModifiedDate: 2022-05-10 16:24:52                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2022 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+
+// 给定一个整数数组 prices ，它的第 i 个元素 prices[i] 是一支给定的股票在第 i 天的价格。
+
+// 设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。
+
+// 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+/**
+ * @param {number} k
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (k, prices) {
+  // 
+};
