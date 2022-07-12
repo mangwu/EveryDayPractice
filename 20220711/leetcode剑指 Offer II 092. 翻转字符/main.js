a9c326034dc718c71893d2e547570b86eb7931c4
mangwu <1185956753@qq.com>
2022-07-12 15:47:36 +0800

    leetcode

--- 20220711/leetcode剑指 Offer II 092. 翻转字符/main.js
+++ 20220711/leetcode剑指 Offer II 092. 翻转字符/main.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: main.js                                                              *
  * @Date: 2022-07-11 09:29:10                                                  *
- * @LastModifiedDate: 2022-07-11 10:01:04                                      *
+ * @LastModifiedDate: 2022-07-12 15:43:27                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2022 mangwu                                                   *
@@ -60,11 +60,6 @@ var minFlipsMonoIncr = function (s) {
  */
 var minFlipsMonoIncr = function (s) {
   // 动态规划
-  // dp[i][0] dp[i][1]分别表示s字符串在第i位时，如果此时的最后一位是0的最小翻转数，以及最后一位是1的最小翻转数
-  // 遍历s，实际最后一位为s[i]
-  // 如果s[i] == "1" 则dp[i][0] = dp[i-1][0] + 1 ，dp[i][1]不变
-  // 如果s[i] == "0" 则dp[i][0] 很前面的一致, dp[i][1] 选取dp[i-1][1] + 1 和dp[i-1][0]的小值
-  // 因为只需要最后一个dp[n-1]，所以不用保存所有动态规划结果,只需保存上一步结果
   let dp0 = 0;
   let dp1 = 0;
   for (const ch of s) {
