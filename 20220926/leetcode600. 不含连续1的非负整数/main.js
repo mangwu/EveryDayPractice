d1315ed410c839bae2b1ab2b6f0cb3c0d095baa1
mangwu <1185956753@qq.com>
2022-09-26 21:32:00 +0800

    leetcode

--- 20220926/leetcode600. 不含连续1的非负整数/main.js
+++ 20220926/leetcode600. 不含连续1的非负整数/main.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: main.js                                                              *
  * @Date: 2022-09-26 16:03:33                                                  *
- * @LastModifiedDate: 2022-09-26 16:50:32                                      *
+ * @LastModifiedDate: 2022-09-26 20:57:40                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2022 mangwu                                                   *
@@ -26,7 +26,7 @@ var findIntegers = function (n) {
       return 1;
     }
     // 判断是否重复计算
-    if (!isLimit && isNum && dp[i] > 0) return dp[i];
+    if (!isLimit && isNum && dp[i] >= 0) return dp[i];
     let res = 0;
     // 前面都没选,现在也可以跳过
     if (!isNum) res = dfs(i + 1, 0, false, false);
@@ -45,4 +45,56 @@ var findIntegers = function (n) {
     return res;
   };
   return dfs(0, 0, true, false);
+};
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var findIntegers = function (n) {
+  const str = n.toString(2);
+  const dp = new Array(str.length).fill(-1).map((_v) => new Array(2).fill(-1));
+  const dfs = (i, pre, isLimit, state) => {
+    if (i === str.length) {
+      return 1;
+    }
+    if (!isLimit && dp[i][state] >= 0) return dp[i][state];
+    let res = 0;
+    let end = isLimit ? str[i] - "0" : 1;
+    for (let d = 0; d <= end; d++) {
+      if (d == pre && pre == 1) {
+        continue;
+      }
+      res += dfs(i + 1, d, isLimit && d == end, d);
+    }
+    if (!isLimit) dp[i][state] = res;
+    return res;
+  };
+  return dfs(0, -1, true, 0);
+};
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var findIntegers = function (n) {
+  const str = n.toString(2);
+  const dp = new Array(str.length).fill(-1).map((_v) => new Array(2).fill(-1));
+  const dfs = (i, pre, isLimit) => {
+    if (i === str.length) {
+      return 1;
+    }
+    if (!isLimit && dp[i][pre] >= 0) return dp[i][pre];
+    let res = 0;
+    let end = isLimit ? str[i] - "0" : 1;
+    for (let d = 0; d <= end; d++) {
+      if (d == pre && pre == 1) {
+        continue;
+      }
+      res += dfs(i + 1, d, isLimit && d == end);
+    }
+    if (!isLimit) dp[i][pre] = res;
+    return res;
+  };
+  return dfs(0, 0, true);
 };
