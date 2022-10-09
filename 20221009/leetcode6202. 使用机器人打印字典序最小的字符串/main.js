cc48b563b07bfd9887511040cb5f85e8575fb382
mangwu <1185956753@qq.com>
2022-10-09 16:52:16 +0800

    leetcode

--- 20221009/leetcode6202. 使用机器人打印字典序最小的字符串/main.js
+++ 20221009/leetcode6202. 使用机器人打印字典序最小的字符串/main.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: main.js                                                              *
  * @Date: 2022-10-09 10:44:19                                                  *
- * @LastModifiedDate: 2022-10-09 11:27:42                                      *
+ * @LastModifiedDate: 2022-10-09 14:46:37                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2022 mangwu                                                   *
@@ -37,7 +37,7 @@ var robotWithString = function (s) {
         if (
           stack.length === 0 ||
           stack[stack.length - 1] < idx ||
-          idx === stack[stack.length - 1] - 1
+          (idx === stack[stack.length - 1] - 1 && idx)
         ) {
           stack.push(idx);
         }
@@ -53,7 +53,7 @@ var robotWithString = function (s) {
   // 在处理最后一个字符时，需要判断第一个字符字符前的非字符是否满足可选条件
   let first = stack[0] - 1;
   const lastCh = s[stack[stack.length - 1] - 1];
-  console.log(first, lastCh)
+  console.log(first, lastCh);
   if (stack.length > 1 && first >= 0 && s[first] <= lastCh) {
     while (s[first] <= lastCh && first >= 0) {
       ans += s[first];
@@ -69,15 +69,6 @@ var robotWithString = function (s) {
   return ans;
 };
 
-// s => azdc => zdc => dc => c  =>
-// t =>      => a   => z  => zd => zdc
-// p =>      =>     => a  => a  => acdz
-
-//
-//  s => hbasaabcmsvba =>
-//  t =>          => ddccbbaa
-//  p =>          =>
-
 // "vzhofnpo"
 
 // "fnopohzv"
@@ -87,3 +78,68 @@ var robotWithString = function (s) {
 // vzhofnpo  => npo   => po    => po   => po    =>
 //           => vzhof => vzhon => vzho => vz    => vzpo
 //                    => f     => fn   => fnoh  => fnoh => fnohopzv
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var robotWithString = function (s) {
+  let ap = new Array(26).fill(0).map((v) => {
+    return [];
+  });
+  const n = s.length;
+  for (let i = 0; i < n; i++) {
+    ap[s[i].charCodeAt() - "a".charCodeAt()].push(i);
+  }
+  // 过滤掉不存在的字符
+  ap = ap.filter((v) => v.length !== 0);
+  const len = ap.length;
+  // 模拟
+  let ans = "";
+  let t = [];
+  let pre = 0;
+  let preIdx = 0;
+  outer: for (let i = 0; i < len; i++) {
+    const m = ap[i].length;
+    for (let k = 0; k < m; k++) {
+      for (let j = pre; j < ap[i][k]; j++) {
+        t.push(s[j]);
+      }
+      if (ap[i][k] == n - 1) {
+        ans += s[ap[i][k]];
+        break outer;
+      }
+      if (ap[i][k] < preIdx) {
+        continue;
+      }
+      preIdx = ap[i][k];
+      pre = ap[i][k] + 1;
+      ans += s[ap[i][k]];
+      // t中的字符能不能选是一个问题
+      // 当前最后一个字符
+      if (k === m - 1) {
+        // 找到符合条件的a，b
+        let a = 1;
+        let b = 0;
+        outer2: for (; a + i < len; a++) {
+          b = 0;
+          for (; b < ap[i + a].length; b++) {
+            if (ap[i + a][b] < ap[i][k]) {
+              continue;
+            } else {
+              break outer2;
+            }
+          }
+        }
+        let nextCh = i < len - 1 && k === m - 1 ? s[ap[i + a][b]] : "A";
+        while (t.length > 0 && t[t.length - 1] <= nextCh) {
+          ans += t.pop();
+        }
+      }
+    }
+  }
+  while (t.length > 0) {
+    ans += t.pop();
+  }
+  return ans;
+};
