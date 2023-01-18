097d6efa924741aaf027c42646f07416c6e23b2e
mangwu <1185956753@qq.com>
2023-01-18 17:32:25 +0800

    leetcode

--- 20230118/leetcode1825. 求出 MK 平均值/main.js
+++ 20230118/leetcode1825. 求出 MK 平均值/main.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: main.js                                                              *
  * @Date: 2023-01-18 09:09:41                                                  *
- * @LastModifiedDate: 2023-01-18 10:53:13                                      *
+ * @LastModifiedDate: 2023-01-18 17:32:15                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2023 mangwu                                                   *
@@ -26,9 +26,12 @@
 // int calculateMKAverage() 对当前的数据流计算并返回 MK 平均数 ，结果需 向下取整到最近的整数 。
 
 class Q {
-  constructor(compare = (a, b) => a - b) {
+  constructor(compare = (a, b) => a - b, m, k) {
     this.compare = compare;
     this.data = [];
+    this.sum = 0;
+    this.m = m;
+    this.k = k;
   }
   binarySearch(val) {
     let left = 0;
@@ -48,9 +51,26 @@ class Q {
     }
     return left;
   }
-  addVal(val) {
-    let idx = this.binarySearch(val);
-    this.data.splice(idx, 0, val);
+  addVal(val, delVal = false) {
+    if (this.data.length < this.m) {
+      this.sum += val;
+    }
+    if (this.data.length === this.m - 1) {
+      let idx = this.binarySearch(val);
+      this.data.splice(idx, 0, val);
+      // 刚好到底可以求和的标准
+      for (let i = 0; i < this.m; i++) {
+        this.sum -= this.data[i];
+        this.sum -= this.data[this.left - i - 1];
+      }
+    } else {
+      // 到达this.m，需要删除delVal
+      let idx = this.binarySearch(delVal);
+      if (idx >= this.k && idx < this.m - this.k) {
+        this.sum -= this.data[idx];
+      }
+      // let idx
+    }
   }
   deleteVal(val) {
     let idx = this.binarySearch(val);
@@ -110,8 +130,8 @@ MKAverage.prototype.calculateMKAverage = function () {
  */
 var MKAverage = function (m, k) {
   // 滑动窗口
-  this.data = new Q((a, b) => a - b);
-  this.sum = 0;
+  this.data = new Q((a, b) => a - b, m, k);
+  this.queue = [];
   this.m = m;
   this.k = k;
 };
@@ -121,13 +141,11 @@ var MKAverage = function (m, k) {
  * @return {void}
  */
 MKAverage.prototype.addElement = function (num) {
-  this.data.addVal(num);
-  if (this.data.length > this.k && this.data.length <= this.m - this.k) {
-    this.sum += num;
-  }
-  if (this.data.length > this.m) {
-    this.sum += this.data[this.data.length - this.k - 1];
-    this.sum -= this.data[this.data.length + this.k - this.m - 1];
+  this.queue.push(num);
+  if (this.queue.length > this.m) {
+    this.data.addVal(num, this.queue[this.queue.length - this.m - 1]);
+  } else {
+    this.data.addVal(num);
   }
 };
 
@@ -139,8 +157,8 @@ MKAverage.prototype.addElement = function (num) {
  * @return {number}
  */
 MKAverage.prototype.calculateMKAverage = function () {
-  if (this.data.length < this.m) return -1;
-  return Math.ceil(this.sum / (this.m - this.k * 2));
+  if (this.data.data.length < this.m) return -1;
+  return Math.ceil(this.data.sum / (this.m - this.k * 2));
 };
 
 /**
