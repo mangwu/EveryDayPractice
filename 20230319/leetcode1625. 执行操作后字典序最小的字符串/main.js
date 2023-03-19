e6064ec87a9d117f9f3391e54f55e067feeea34d
mangwu <1185956753@qq.com>
2023-03-20 00:17:05 +0800

    leetcode

--- 20230319/leetcode1625. 执行操作后字典序最小的字符串/main.js
+++ 20230319/leetcode1625. 执行操作后字典序最小的字符串/main.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: main.js                                                              *
  * @Date: 2023-03-19 13:52:55                                                  *
- * @LastModifiedDate: 2023-03-19 15:37:20                                      *
+ * @LastModifiedDate: 2023-03-20 00:14:38                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2023 mangwu                                                   *
@@ -56,12 +56,117 @@ var findLexSmallestString = function (s, a, b) {
  * @return {string}
  */
 var findLexSmallestString = function (s, a, b) {
-  // 如果b是偶数，那么
-}
+  // 先进行轮转
+  const n = s.length;
+  const set = new Set([s]);
+  let start = b;
+  while (true) {
+    let cur = "";
+    for (let i = 0; i < n; i++) {
+      cur += s[(i + start) % n];
+    }
+    if (set.has(cur)) break;
+    set.add(cur);
+    start += b;
+  }
+  const arr = [...set];
+  for (const item of arr) {
+    for (let i = 1; i <= 9; i++) {
+      // 添加第10次就循环了
+      let cur = "";
+      let cur2 = "";
+      for (let j = 1; j < n; j += 2) {
+        cur += item[j - 1] + ((parseInt(item[j]) + i * a) % 10);
+      }
+      set.add(cur);
+      for (let j = 0; j < n; j += 2) {
+        cur2 += ((parseInt(item[j]) + i * a) % 10) + item[j + 1];
+      }
+      set.add(cur2);
+    }
+  }
+  return [...set].sort((a, b) => a - b)[0];
+};
 
 // 1 1 2 3 4 5
 // 4 5 1 1 2 3
 // 2 3 4 5 1 1
-     
+
 // 奇数轮转
-// 
+//
+
+/**
+ * @param {string} s
+ * @param {number} a
+ * @param {number} b
+ * @return {string}
+ */
+var findLexSmallestString = function (s, a, b) {
+  // 先进行轮转
+  const n = s.length;
+  const vis = new Array(n).fill(false);
+  let res = s;
+  s = s + s; // 方便轮转
+  // 进行轮转
+  for (let i = 0; !vis[i]; i = (i + b) % n) {
+    vis[i] = true;
+    for (let j = 0; j < 10; j++) {
+      // b如果是偶数，就只会对字符串上的偶数位进行翻转
+      let kLimit = b % 2 === 0 ? 0 : 9;
+      for (let k = 0; k <= kLimit; k++) {
+        const t = [...s.slice(i, i + n)];
+        for (let p = 1; p < n; p += 2) {
+          t[p] = String.fromCharCode(
+            "0".charCodeAt() +
+              ((t[p].charCodeAt() - "0".charCodeAt() + j * a) % 10)
+          );
+        }
+        for (let p = 0; p < n; p += 2) {
+          t[p] = String.fromCharCode(
+            "0".charCodeAt() +
+              ((t[p].charCodeAt() - "0".charCodeAt() + k * a) % 10)
+          );
+        }
+        const tStr = t.join("");
+        if (tStr < res) {
+          res = tStr;
+        }
+      }
+    }
+  }
+  return res;
+};
+
+/**
+ * @param {string} s
+ * @param {number} a
+ * @param {number} b
+ * @return {string}
+ */
+var findLexSmallestString = function (s, a, b) {
+  // 先进行轮转
+  const n = s.length;
+  const set = new Set();
+  let res = s;
+  let start = 0;
+  // 进行轮转
+  while (true) {
+    let curS = "";
+    for (let i = 0; i < n; i++) curS += s[(i + start) % n];
+    if (set.has(curS)) break;
+    set.add(curS);
+    start += b;
+    // 进行累加，如果b是偶数，则只能对奇数进行增加
+    for (let j = 0; j < 10; j++) {
+      let kLimit = b % 2 ? 9 : 0;
+      for (let k = 0; k <= kLimit; k++) {
+        let str = "";
+        for (let p = 0; p < n; p++) {
+          str += (parseInt(curS[p]) + (((p % 2 === 1 ? j : k) * a) % 10)) % 10;
+        }
+        if (str < res) res = str;
+      }
+    }
+  }
+  return res;
+};
