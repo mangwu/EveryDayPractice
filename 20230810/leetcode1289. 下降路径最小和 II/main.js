cd3d3bdcc87284f3bd1545632fef9dd77b659489
mangwu <1185956753@qq.com>
2023-08-11 10:30:58 +0800

    leetcode

---
+++ 20230810/leetcode1289. 下降路径最小和 II/main.js
@@ -0,0 +1,26 @@
+/*
+ * @Author: mangwu                                                             *
+ * @File: main.js                                                              *
+ * @Date: 2023-08-10 08:54:47                                                  *
+ * @LastModifiedDate: 2023-08-10 08:56:51                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2023 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+
+
+// 给你一个 n x n 整数矩阵 grid ，请你返回 非零偏移下降路径 数字和的最小值。
+
+// 非零偏移下降路径 定义为：从 grid 数组中的每一行选择一个数字，且按顺序选出来的数字中，相邻数字不在原数组的同一列。
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minFallingPathSum = function(grid) {
+  
+};
