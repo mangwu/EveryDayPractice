1966b1cae4e2a2bb6b3e0814ecf415c8324aee8a
mangwu <1185956753@qq.com>
2023-10-07 10:19:02 +0800

    leetcode

--- 20231007/leetcode901. 股票价格跨度/main.js
+++ 20231007/leetcode901. 股票价格跨度/main.js
@@ -41,3 +41,31 @@ StockSpanner.prototype.next = function (price) {
  * var obj = new StockSpanner()
  * var param_1 = obj.next(price)
  */
+
+var StockSpanner = function () {
+  this.stack = [];
+};
+
+/**
+ * @param {number} price
+ * @return {number}
+ */
+StockSpanner.prototype.next = function (price) {
+  // 单调栈
+  // 求解price的下一个更大的元素
+  let ans = 1;
+  while (
+    this.stack.length > 0 &&
+    price >= this.stack[this.stack.length - 1][0]
+  ) {
+    ans += this.stack.pop()[1];
+  }
+  this.stack.push([price, ans]);
+  return ans;
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * var obj = new StockSpanner()
+ * var param_1 = obj.next(price)
+ */
