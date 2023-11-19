77abebddfc13f9ecb7bb8ea04f58b2a60c13954e
mangwu <1185956753@qq.com>
2023-11-19 22:34:20 +0800

    leetcode

---
+++ 20231119/leetcode689. 三个无重叠子数组的最大和/main.js
@@ -0,0 +1,27 @@
+/*
+ * @Author: mangwu                                                             *
+ * @File: main.js                                                              *
+ * @Date: 2023-11-19 22:25:32                                                  *
+ * @LastModifiedDate: 2023-11-19 22:34:11                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2023 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+
+
+// 给你一个整数数组 nums 和一个整数 k ，找出三个长度为 k 、互不重叠、且全部数字和（3 * k 项）最大的子数组，并返回这三个子数组。
+
+// 以下标的数组形式返回结果，数组中的每一项分别指示每个子数组的起始位置（下标从 0 开始）。如果有多个结果，返回字典序最小的一个。
+
+ /**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSumOfThreeSubarrays = function(nums, k) {
+  // const 
+};
