d44d2492a131906c0ce67436a42ccbf1c547767b
mangwu <1185956753@qq.com>
2023-12-14 17:07:47 +0800

    leetcode

---
+++ 20231214/leetcode2132. 用邮票贴满网格图/main.js
@@ -0,0 +1,22 @@
+// 给你一个 m x n 的二进制矩阵 grid ，每个格子要么为 0 （空）要么为 1 （被占据）。
+
+// 给你邮票的尺寸为 stampHeight x stampWidth 。我们想将邮票贴进二进制矩阵中，且满足以下 限制 和 要求 ：
+
+// 覆盖所有 空 格子。
+// 不覆盖任何 被占据 的格子。
+// 我们可以放入任意数目的邮票。
+// 邮票可以相互有 重叠 部分。
+// 邮票不允许 旋转 。
+// 邮票必须完全在矩阵 内 。
+// 如果在满足上述要求的前提下，可以放入邮票，请返回 true ，否则返回 false 。
+
+
+/**
+ * @param {number[][]} grid
+ * @param {number} stampHeight
+ * @param {number} stampWidth
+ * @return {boolean}
+ */
+var possibleToStamp = function(grid, stampHeight, stampWidth) {
+  
+};
