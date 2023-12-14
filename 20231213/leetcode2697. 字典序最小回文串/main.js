f21e17fb7324ae4e1261929e9857e83de2ce24da
mangwu <1185956753@qq.com>
2023-12-14 08:50:10 +0800

    leetcode

--- 20231213/leetcode2697. 字典序最小回文串/main.js
+++ 20231213/leetcode2697. 字典序最小回文串/main.js
@@ -1,78 +1,17 @@
-// Dota2 的世界里有两个阵营：Radiant（天辉）和 Dire（夜魇）
-
-// Dota2 参议院由来自两派的参议员组成。现在参议院希望对一个 Dota2 游戏里的改变作出决定。他们以一个基于轮为过程的投票进行。在每一轮中，每一位参议员都可以行使两项权利中的 一 项：
-
-// 禁止一名参议员的权利：参议员可以让另一位参议员在这一轮和随后的几轮中丧失 所有的权利 。
-// 宣布胜利：如果参议员发现有权利投票的参议员都是 同一个阵营的 ，他可以宣布胜利并决定在游戏中的有关变化。
-// 给你一个字符串 senate 代表每个参议员的阵营。字母 'R' 和 'D'分别代表了 Radiant（天辉）和 Dire（夜魇）。然后，如果有 n 个参议员，给定字符串的大小将是 n。
-
-// 以轮为基础的过程从给定顺序的第一个参议员开始到最后一个参议员结束。这一过程将持续到投票结束。所有失去权利的参议员将在过程中被跳过。
-
-// 假设每一位参议员都足够聪明，会为自己的政党做出最好的策略，你需要预测哪一方最终会宣布胜利并在 Dota2 游戏中决定改变。输出应该是 "Radiant" 或 "Dire" 。
-
-class Q {
-  constructor() {
-    this.items = {};
-    this.lowest = 0;
-    this.count = 0;
-  }
-  size() {
-    return this.count;
-  }
-  isEmpty() {
-    return this.size() === 0;
-  }
-  enqueue(value) {
-    this.items[this.lowest + this.count++] = value;
-  }
-  peek() {
-    if (this.isEmpty()) return undefined;
-    return this.items[this.lowest];
-  }
-  dequeue() {
-    if (this.isEmpty()) return undefined;
-    const res = this.items[this.lowest++];
-    this.count--;
-    delete this.items[this.lowest - 1];
-    return res;
-  }
-}
-
 /**
- * @param {string} senate
+ * @param {string} s
  * @return {string}
  */
-var predictPartyVictory = function (senate) {
-  // 被禁止的人数
-  let rb = 0;
-  let db = 0;
-  // 统计个数
-  let R = 0;
-  let D = 0;
-  const q = new Q();
-  for (const ch of senate) {
-    ch === "R" ? R++ : D++;
-    q.enqueue(ch);
-  }
-  while (R && D) {
-    const cur = q.dequeue();
-    if (cur === "R") {
-      if (rb) {
-        rb--;
-        R--;
-      } else {
-        db++;
-        q.enqueue(cur);
-      }
+var makeSmallestPalindrome = function (s) {
+  const n = s.length;
+  const arr = s.split("");
+  for (let i = 0; i < n / 2; i++) {
+    let right = n - i - 1;
+    if (arr[i] < arr[right]) {
+      arr[right] = arr[i];
     } else {
-      if (db) {
-        db--;
-        D--;
-      } else {
-        rb++;
-        q.enqueue(cur);
-      }
+      arr[i] = arr[right];
     }
   }
-  return R ? "Radiant" : "Dire";
+  return arr.join("");
 };
