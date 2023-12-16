c5c8fb854b2e8b4afa98f7157c51dcfb3cb8bd7f
mangwu <1185956753@qq.com>
2023-12-16 23:37:37 +0800

    leetcode

---
+++ 20231216/leetcode2276. 统计区间中的整数数目/main.js
@@ -0,0 +1,54 @@
+/*
+ * @Author: mangwu                                                             *
+ * @File: main.js                                                              *
+ * @Date: 2023-12-16 23:02:22                                                  *
+ * @LastModifiedDate: 2023-12-16 23:07:08                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2023 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+
+
+// 给你区间的 空 集，请你设计并实现满足要求的数据结构：
+
+// 新增：添加一个区间到这个区间集合中。
+// 统计：计算出现在 至少一个 区间中的整数个数。
+// 实现 CountIntervals 类：
+
+// CountIntervals() 使用区间的空集初始化对象
+// void add(int left, int right) 添加区间 [left, right] 到区间集合之中。
+// int count() 返回出现在 至少一个 区间中的整数个数。
+// 注意：区间 [left, right] 表示满足 left <= x <= right 的所有整数 x 。
+
+
+
+var CountIntervals = function() {
+  
+};
+
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {void}
+ */
+CountIntervals.prototype.add = function(left, right) {
+
+};
+
+/**
+ * @return {number}
+ */
+CountIntervals.prototype.count = function() {
+
+};
+
+/**
+ * Your CountIntervals object will be instantiated and called as such:
+ * var obj = new CountIntervals()
+ * obj.add(left,right)
+ * var param_2 = obj.count()
+ */
