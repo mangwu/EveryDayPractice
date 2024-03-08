efe8ca373557614402789619713bd57f6d7bae12
mangwu <1185956753@qq.com>
2024-03-08 17:35:17 +0800

    leetcode

---
+++ 20240308/leetcode456. 132 模式/main.js
@@ -0,0 +1,11 @@
+// 给你一个整数数组 nums ，数组中共有 n 个整数。132 模式的子序列 由三个整数 nums[i]、nums[j] 和 nums[k] 组成，并同时满足：i < j < k 和 nums[i] < nums[k] < nums[j] 。
+
+// 如果 nums 中存在 132 模式的子序列 ，返回 true ；否则，返回 false 。
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var find132pattern = function(nums) {
+  
+};
