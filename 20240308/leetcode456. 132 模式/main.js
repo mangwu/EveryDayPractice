2b46c381f309f09b0f9d9cfdb988a1fb516bc4c8
mangwu <1185956753@qq.com>
2024-03-14 17:42:59 +0800

    leetcode

--- 20240308/leetcode456. 132 模式/main.js
+++ 20240308/leetcode456. 132 模式/main.js
@@ -29,6 +29,100 @@ var find132pattern = function (nums) {
 // 1 2 3 4   -4
 // (-4) (-3)     -5
 // (-5) (-1)
-// 
+//
 // [3,5,0,3]
 // [3,5,0,4]
+class PQ {
+  constructor(compare = (a, b) => a - b) {
+    this.items = [];
+    this.compareFn = compare;
+  }
+  compare(a, b) {
+    return this.compareFn(this.items[a], this.items[b]);
+  }
+  swap(a, b) {
+    [this.items[a], this.items[b]] = [this.items[b], this.items[a]];
+  }
+  getParentIdx(idx) {
+    return Math.floor((idx - 1) / 2);
+  }
+  getLeftIdx(idx) {
+    return idx * 2 + 1;
+  }
+  getRightIdx(idx) {
+    return idx * 2 + 2;
+  }
+  isEmpty() {
+    return this.size() === 0;
+  }
+  size() {
+    return this.items.length;
+  }
+  peek() {
+    if (this.isEmpty()) return undefined;
+    return this.items[0];
+  }
+  insert(val) {
+    if (!val) return false;
+    this.items.push(val);
+    this.shiftUp();
+    return true;
+  }
+  shiftUp() {
+    let idx = this.size() - 1;
+    let parentIdx = this.getParentIdx(idx);
+    while (parentIdx >= 0 && this.compare(idx, parentIdx) < 0) {
+      this.swap(idx, parentIdx);
+      idx = parentIdx;
+      parentIdx = this.getParentIdx(idx);
+    }
+  }
+  poll() {
+    if (this.isEmpty()) return undefined;
+    const size = this.size();
+    if (size === 1) return this.items.pop();
+    this.swap(size - 1, 0);
+    const res = this.items.pop();
+    this.shiftDown();
+    return res;
+  }
+  shiftDown() {
+    let idx = 0;
+    const size = this.size();
+    let temp = idx;
+    while (idx < size) {
+      const leftIdx = this.getLeftIdx(idx);
+      const rightIdx = this.getRightIdx(idx);
+      if (leftIdx < size && this.compare(idx, leftIdx) > 0) idx = leftIdx;
+      if (rightIdx < size && this.compare(idx, rightIdx) > 0) idx = rightIdx;
+      if (idx !== temp) {
+        this.swap(idx, temp);
+        temp = idx;
+      } else break;
+    }
+  }
+}
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var find132pattern = function (nums) {
+  // 单调栈陷阱
+  // 枚举3
+  const n = nums.length;
+  let leftMin = nums[0];
+  const pq = new PQ((a, b) => nums[a] - nums[b]);
+  for (let i = 1; i < n; i++) pq.insert(i);
+  for (let i = 1; i < n; i++) {
+    // 弹出索引在[0,i]的值
+    while (!pq.isEmpty() && pq.peek() <= i) {
+      pq.poll();
+    }
+    if (nums[i] > leftMin) {
+      if (pq.isEmpty()) return false;
+      else if (nums[pq.peek()] < nums[i]) return true;
+    }
+    leftMin = Math.min(leftMin, nums[i]);
+  }
+  return false;
+};
