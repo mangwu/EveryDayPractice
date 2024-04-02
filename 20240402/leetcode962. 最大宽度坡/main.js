b109ca9dd3c2659797613b40b52ffec4c016b2e9
mangwu <1185956753@qq.com>
2024-04-02 17:34:58 +0800

    leetcode

---
+++ 20240402/leetcode962. 最大宽度坡/main.js
@@ -0,0 +1,13 @@
+// 给定一个整数数组 A，坡是元组 (i, j)，其中  i < j 且 A[i] <= A[j]。这样的坡的宽度为 j - i。
+
+// 找出 A 中的坡的最大宽度，如果不存在，返回 0
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxWidthRamp = function (nums) {
+  const n = nums.length;
+  const stack = [];
+  
+};
