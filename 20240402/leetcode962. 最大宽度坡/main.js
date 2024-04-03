6af33c0ca1b44f1085c0c3919854aecb34bd0424
mangwu <1185956753@qq.com>
2024-04-03 10:34:57 +0800

    leetcode

--- 20240402/leetcode962. 最大宽度坡/main.js
+++ 20240402/leetcode962. 最大宽度坡/main.js
@@ -8,6 +8,36 @@
  */
 var maxWidthRamp = function (nums) {
   const n = nums.length;
-  const stack = [];
-  
+  let ans = 0;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if (nums[i] <= nums[j]) {
+        ans = Math.max(ans, j - i);
+      }
+    }
+  }
+  return ans;
 };
+// 暴力解法
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxWidthRamp = function (nums) {
+  const n = nums.length;
+  let ans = 0;
+  // 排序
+  const sorted = new Array(n)
+    .fill(0)
+    .map((_v, i) => i)
+    .sort((a, b) => (nums[a] !== nums[b] ? nums[a] - nums[b] : a - b));
+  let minIdx = sorted[0];
+  for (let i = 1; i < n; i++) {
+    ans = Math.max(ans, sorted[i] - minIdx);
+    minIdx = Math.min(minIdx, sorted[i]);
+  }
+  return ans;
+};
+
+// 132
