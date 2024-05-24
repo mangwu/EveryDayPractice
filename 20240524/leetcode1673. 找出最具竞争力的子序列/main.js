67e7f243e58512fd277f3c4a79382272cd1412fc
mangwu <1185956753@qq.com>
2024-05-24 09:42:19 +0800

    leetcode

---
+++ 20240524/leetcode1673. 找出最具竞争力的子序列/main.js
@@ -0,0 +1,29 @@
+// 给你一个整数数组 nums 和一个正整数 k ，返回长度为 k 且最具 竞争力 的 nums 子序列。
+
+// 数组的子序列是从数组中删除一些元素（可能不删除元素）得到的序列。
+
+// 在子序列 a 和子序列 b 第一个不相同的位置上，如果 a 中的数字小于 b 中对应的数字，那么我们称子序列 a 比子序列 b（相同长度下）更具 竞争力 。 例如，[1,3,4] 比 [1,3,5] 更具竞争力，在第一个不相同的位置，也就是最后一个位置上， 4 小于 5 。
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var mostCompetitive = function (nums, k) {
+  // 单调栈
+  const n = nums.length;
+  let remain = n - k; // 要移除的元素个数
+  const stack = [];
+  for (const num of nums) {
+    while (remain && stack.length && stack[stack.length - 1] > num) {
+      stack.pop();
+      remain--;
+    }
+    if (stack.length >= k) {
+      remain--;
+    } else {
+      stack.push(num);
+    }
+  }
+  return stack;
+};
