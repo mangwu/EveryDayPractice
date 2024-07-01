97703201acf75011e3a0889bf02a0e1c67555870
mangwu <1185956753@qq.com>
2024-07-01 17:41:20 +0800

    leetcode

---
+++ 20240701/leetcode2065. 最大化一张图中的路径价值/main.js
@@ -0,0 +1,32 @@
+/*
+ * @Author: mangwu                                                             *
+ * @File: main.js                                                              *
+ * @Date: 2024-07-01 08:53:41                                                  *
+ * @LastModifiedDate: 2024-07-01 09:12:55                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2024 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+
+
+// 给你一张 无向 图，图中有 n 个节点，节点编号从 0 到 n - 1 （都包括）。同时给你一个下标从 0 开始的整数数组 values ，其中 values[i] 是第 i 个节点的 价值 。同时给你一个下标从 0 开始的二维整数数组 edges ，其中 edges[j] = [uj, vj, timej] 表示节点 uj 和 vj 之间有一条需要 timej 秒才能通过的无向边。最后，给你一个整数 maxTime 。
+
+// 合法路径 指的是图中任意一条从节点 0 开始，最终回到节点 0 ，且花费的总时间 不超过 maxTime 秒的一条路径。你可以访问一个节点任意次。一条合法路径的 价值 定义为路径中 不同节点 的价值 之和 （每个节点的价值 至多 算入价值总和中一次）。
+
+// 请你返回一条合法路径的 最大 价值。
+
+// 注意：每个节点 至多 有 四条 边与之相连。
+
+/**
+ * @param {number[]} values
+ * @param {number[][]} edges
+ * @param {number} maxTime
+ * @return {number}
+ */
+var maximalPathQuality = function(values, edges, maxTime) {
+  
+};
