265ee04742c52c9cc2f9df7a4c8554142e796795
mangwu <1185956753@qq.com>
2024-08-27 17:36:03 +0800

    leetcode

---
+++ 20240827/leetcode3134. 找出唯一性数组的中位数/main.js
@@ -0,0 +1,34 @@
+/*
+ * @Author: mangwu                                                             *
+ * @File: main.js                                                              *
+ * @Date: 2024-08-27 09:01:16                                                  *
+ * @LastModifiedDate: 2024-08-27 09:03:08                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2024 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+
+
+// 给你一个整数数组 nums 。数组 nums 的 唯一性数组 是一个按元素从小到大排序的数组，包含了 nums 的所有
+// 非空子数组中
+// 不同元素的个数。
+
+// 换句话说，这是由所有 0 <= i <= j < nums.length 的 distinct(nums[i..j]) 组成的递增数组。
+
+// 其中，distinct(nums[i..j]) 表示从下标 i 到下标 j 的子数组中不同元素的数量。
+
+// 返回 nums 唯一性数组 的 中位数 。
+
+// 注意，数组的 中位数 定义为有序数组的中间元素。如果有两个中间元素，则取值较小的那个。
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var medianOfUniquenessArray = function(nums) {
+  
+};
