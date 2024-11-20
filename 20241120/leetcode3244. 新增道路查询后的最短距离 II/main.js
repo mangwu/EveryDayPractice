fd2783cf31fb501fdd2880daf4fc2eb417026d0f
mangwu <1185956753@qq.com>
2024-11-20 18:04:12 +0800

    leetcode

---
+++ 20241120/leetcode3244. 新增道路查询后的最短距离 II/main.js
@@ -0,0 +1,33 @@
+/*
+ * @Author: mangwu                                                             *
+ * @File: main.js                                                              *
+ * @Date: 2024-11-20 17:56:45                                                  *
+ * @LastModifiedDate: 2024-11-20 17:57:05                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2024 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+
+
+// 给你一个整数 n 和一个二维整数数组 queries。
+
+// 有 n 个城市，编号从 0 到 n - 1。初始时，每个城市 i 都有一条单向道路通往城市 i + 1（ 0 <= i < n - 1）。
+
+// queries[i] = [ui, vi] 表示新建一条从城市 ui 到城市 vi 的单向道路。每次查询后，你需要找到从城市 0 到城市 n - 1 的最短路径的长度。
+
+// 所有查询中不会存在两个查询都满足 queries[i][0] < queries[j][0] < queries[i][1] < queries[j][1]。
+
+// 返回一个数组 answer，对于范围 [0, queries.length - 1] 中的每个 i，answer[i] 是处理完前 i + 1 个查询后，从城市 0 到城市 n - 1 的最短路径的长度。
+
+/**
+ * @param {number} n
+ * @param {number[][]} queries
+ * @return {number[]}
+ */
+var shortestDistanceAfterQueries = function(n, queries) {
+  
+};
