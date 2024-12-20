21411ae01b37db55380982770846bf39b90bae20
mangwu <1185956753@qq.com>
2024-12-20 17:34:04 +0800

    leetcode

---
+++ 20241220/leetcode3388. 统计数组中的美丽分割/main.js
@@ -0,0 +1,25 @@
+// 给你一个整数数组 nums 。
+
+// 如果数组 nums 的一个分割满足以下条件，我们称它是一个 美丽 分割：
+
+// 数组 nums 分为三段 
+// 非空子数组
+// ：nums1 ，nums2 和 nums3 ，三个数组 nums1 ，nums2 和 nums3 按顺序连接可以得到 nums 。
+// 子数组 nums1 是子数组 nums2 的 
+// 前缀
+//  或者 nums2 是 nums3 的 
+// 前缀
+// 。
+// 请你返回满足以上条件的分割 数目 。
+
+// 子数组 指的是一个数组里一段连续 非空 的元素。
+
+// 前缀 指的是一个数组从头开始到中间某个元素结束的子数组。
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var beautifulSplits = function(nums) {
+    
+};
